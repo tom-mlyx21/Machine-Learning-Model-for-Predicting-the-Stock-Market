66251bd3f9f135f1125a2ab58c31418dfeea105a
Tom40 <tom40@aber.ac.uk>
2024-02-25 22:38:26 +0000

    Itterator Commit

--- Sentiment Extractor.py
+++ Sentiment Extractor.py
@@ -2,32 +2,29 @@
 import pandas as pd
 import numpy as np
 import nltk
-pd.options.mode.chained_assignment = None # default='warn'
+
+pd.options.mode.chained_assignment = None  # default='warn'
 
 # Load the model into pandas
 first_data = pd.read_csv('Combined_News_DJIA.csv')
 # print(first_data.iloc[0])
 
-# Using a ready made sentiment extractor to test against my model
+# Using a ready-made sentiment extractor to test against my model
 nltk.download('vader_lexicon')
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-text = "the start and end of the book"
-scores = SentimentIntensityAnalyzer().polarity_scores(text)
-#print(scores)
-
-# Use Pandas to itterate over the data entries
+# Use Pandas to iterate over the data entries
 # Build a new pandas dataframe with the sentiment scores + length
 sentiment_data = first_data.copy()
+"""
+counter = 0
 for y in first_data.index:
     for x in first_data.columns:
-        #print(x)
         if x != 'Date' and x != 'Label':
             target = first_data[x][y]
-            #print(target)
             hold = []
-            print(target)
-            if target != "nan":
+            # Can't encode floats so only strings accepted (Loss of 1 in 250 ish entries)
+            if type(target) == str:
                 scores = SentimentIntensityAnalyzer().polarity_scores(target)
                 # Migrate the vader object to an array, to better suit pd
                 hold.append(len(target))
@@ -35,6 +32,25 @@ for y in first_data.index:
                 for i in scores.values():
                     hold.append(i)
                 sentiment_data[x][y] = hold
-                #print(hold)
-                # print(scores)
-print(sentiment_data.iloc[1])
+    counter+=1
+    print(counter)
+"""
+# Since the original dataset is too large, I will only use the first 50 entries to test the code
+counter = 0
+for y in range(50):
+    for x in first_data.columns:
+        if x != 'Date' and x != 'Label':
+            target = first_data[x][y]
+            hold = []
+            # Can't encode floats so only strings accepted (Loss of 1 in 250 ish entries)
+            if type(target) == str:
+                scores = SentimentIntensityAnalyzer().polarity_scores(target)
+                # Migrate the vader object to an array, to better suit pd
+                hold.append(len(target))
+                hold.append(target.count(' ') + 1)
+                for i in scores.values():
+                    hold.append(i)
+                sentiment_data[x][y] = hold
+    counter += 1
+    print(counter)
+print(sentiment_data.iloc[2:50])
