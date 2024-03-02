052024c8b03b916e6f1cc06561756ebeefd24419
Tom40 <tom40@aber.ac.uk>
2024-03-02 22:06:40 +0000

    NGram Correction, Punctuation feature, Partial Entity

--- Sentiment Extractor.py
+++ Sentiment Extractor.py
@@ -2,17 +2,18 @@
 import pandas as pd
 import numpy as np
 import nltk
+import string
 from nltk.lm import NgramCounter
 from nltk.util import ngrams
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report
 
+
 pd.options.mode.chained_assignment = None  # default='warn'
 
 # Load the model into pandas
 first_data = pd.read_csv('Combined_News_DJIA.csv')
-# print(first_data.iloc[0])
 
 # Using a ready-made sentiment extractor to test against my model
 nltk.download('vader_lexicon')
@@ -21,6 +22,7 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 # Use Pandas to iterate over the data entries
 # Build a new pandas dataframe with the sentiment scores + length
 sentiment_data = first_data.copy()
+print(sentiment_data.iloc[0][6])
 """
 counter = 0
 for y in first_data.index:
@@ -53,17 +55,34 @@ for y in range(50):
                 # Migrate the vader object to an array, to better suit pd
                 hold.append(len(target))
                 hold.append(target.count(' ') + 1)
-                #Counting ngrams in text
-                text_unigrams = [ngrams(sent, 1) for sent in target]
-                text_bigrams = [ngrams(sent, 2) for sent in target]
-                text_trigrams = [ngrams(sent, 3) for sent in target]
-                count = NgramCounter(text_unigrams+text_bigrams+text_trigrams)
+                # totaling ngrams in text
+                sent = target.split()
+                uni, bi, tri = 0, 0, 0
+                for t in sent:
+                    if len(t) == 1 and t != "'" and t != '-' and t != '"':
+                        uni += 1
+                        print("Unigram: ", t)
+                    if len(t) == 2 and t != "--":
+                        bi += 1
+                        print("Bigram: ", t)
+                    if len(t) == 3:
+                        tri += 1
+                        print("Trigram: ", t)
+                print(uni, bi, tri)
+                # punctuation count
+                punc = 0
+                for c in target:
+                    if c in string.punctuation and c != '"':
+                        punc += 1
                 for i in scores.values():
                     hold.append(i)
                 sentiment_data[x][y] = hold
-                hold.append(count.N())
+                hold.append(uni)
+                hold.append(bi)
+                hold.append(tri)
+                hold.append(punc)
     counter += 1
-    print(counter)
+print(hold)
 print(sentiment_data.iloc[2:50])
 
 
@@ -71,18 +90,18 @@ print(sentiment_data.iloc[2:50])
 # A new dataset will be built using new column headings
 # Source, Length, Word Count, Positive, Negative, Neutral, Compound
 
-clean_data = pd.DataFrame(columns=['Source', 'Length', 'Word Count', 'Positive', 'Negative', 'Neutral', 'Compound', 'Label', 'Avg WordLen', 'N-Grams', 'Polarity Shift', 'Punctuation', 'Entities'])
+clean_data = pd.DataFrame(columns=['Source', 'Length', 'Word Count', 'Positive', 'Negative', 'Neutral', 'Compound', 'Label', 'Avg WordLen', 'UniGrams', 'BiGrams', 'TriGrams', 'Punctuation', 'Entities'])
 for x in sentiment_data.columns:
     if x != 'Date' and x != 'Label':
         for y in range(5):
             hold = sentiment_data[x][y]
             if type(hold) == list:
-                set = {'Source': x[3:], 'Length': hold[0], 'Word Count': hold[1], 'Positive': hold[2], 'Negative': hold[3], 'Neutral': hold[4], 'Compound': hold[5], 'Label': sentiment_data['Label'][y], 'Avg WordLen': int(hold[0]/hold[1]), 'N-Grams': int(hold[6])}
+                set = {'Source': x[3:], 'Length': hold[0], 'Word Count': hold[1], 'Positive': hold[2], 'Negative': hold[3], 'Neutral': hold[4], 'Compound': hold[5], 'Label': sentiment_data['Label'][y], 'Avg WordLen': int(hold[0]/hold[1]), 'UniGrams': int(hold[6]), 'BiGrams': int(hold[7]), 'TriGrams': int(hold[8]), 'Punctuation': int(hold[9])}
                 clean_data.loc[len(clean_data)] = set
 print(clean_data.head())
 
 # Time For Training
-X = clean_data[['Source', 'Length', 'Word Count', 'Positive', 'Negative', 'Neutral', 'Compound', 'Avg WordLen', 'N-Grams']]
+X = clean_data[['Source', 'Length', 'Word Count', 'Positive', 'Negative', 'Neutral', 'Compound', 'Avg WordLen', 'UniGrams', 'BiGrams', 'TriGrams', 'Punctuation']]
 Y = clean_data['Label']
 Xtrain, Xtest, Ytrain, Ytest = train_test_split(X,Y, test_size=.3, random_state=0)
 clf = GaussianNB()
