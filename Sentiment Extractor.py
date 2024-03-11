1596d0c3ee0854cfd32b9f62d337bdb624e77877
Tom <tom40@aber.ac.uk>
2024-03-11 23:29:51 +0000

    Sentiment extraction data file extension New Data Indexing problem

--- Sentiment Extractor.py
+++ Sentiment Extractor.py
@@ -1,12 +1,23 @@
+import xgboost as xgb
 import pandas as pd
+import numpy as np
 import nltk
 import string
+from nltk import word_tokenize
+from nltk.lm import NgramCounter
+from nltk.util import ngrams
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold, cross_val_score
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
 
-nltk.download('punkt')
+'''nltk.download('punkt')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
-nltk.download('vader_lexicon')
+nltk.download('vader_lexicon')'''
 
 
 pd.options.mode.chained_assignment = None  # default='warn'
@@ -31,13 +42,14 @@ else:
 for y in range(boundary):
     print("index:",  counter)
     for x in first_data.columns:
-        if x != 'Date' and x != 'Label':
+        if x != 'Date':
             target = first_data[x][y]
             hold = []
             # Can't encode floats so only strings accepted (Loss of 1 in 250 ish entries)
             if type(target) == str:
                 scores = SentimentIntensityAnalyzer().polarity_scores(target)
                 # Migrate the vader object to an array, to better suit pd
+                hold.append(counter)
                 hold.append(len(target))
                 hold.append(target.count(' ') + 1)
                 # totaling ngrams in text
@@ -57,13 +69,17 @@ for y in range(boundary):
                         punc += 1
                 for i in scores.values():
                     hold.append(i)
-                sentiment_data[x][y] = hold
+                #for i in scores.keys():
+                #    hold.append(scores[i])
                 hold.append(uni)
                 hold.append(bi)
                 hold.append(tri)
                 hold.append(punc)
                 # include the entity count in array
                 hold.append(len(nltk.word_tokenize(target)))
+                hold.append(first_data.iloc[y]['Label'])
+                # The hold temporarily assumes all metrics taken about sentiment. Then passed on to the
+                sentiment_data.at[counter, x] = hold
     counter += 1
 print(hold)
 print(sentiment_data.iloc[2:50])
@@ -73,14 +89,14 @@ print(sentiment_data.iloc[2:50])
 # A new dataset will be built using new column headings
 # Source, Length, Word Count, Positive, Negative, Neutral, Compound
 
-clean_data = pd.DataFrame(columns=['Source', 'Length', 'Word Count', 'Positive', 'Negative', 'Neutral', 'Compound', 'Label', 'Avg WordLen', 'UniGrams', 'BiGrams', 'TriGrams', 'Punctuation', 'Entities'])
+clean_data = pd.DataFrame(columns=['Source', 'Length', 'Word Count', 'Positive', 'Negative', 'Neutral', 'Compound', 'Avg WordLen', 'UniGrams', 'BiGrams', 'TriGrams', 'Punctuation', 'Entities', 'Label'])
 for x in sentiment_data.columns:
     if x != 'Date' and x != 'Label':
-        for y in range(5):
+        for y in range(boundary):
             hold = sentiment_data[x][y]
             if type(hold) == list:
-                set = {'Source': x[3:], 'Length': hold[0], 'Word Count': hold[1], 'Positive': hold[2], 'Negative': hold[3], 'Neutral': hold[4], 'Compound': hold[5], 'Label': sentiment_data['Label'][y], 'Avg WordLen': int(hold[0]/hold[1]), 'UniGrams': int(hold[6]), 'BiGrams': int(hold[7]), 'TriGrams': int(hold[8]), 'Punctuation': int(hold[9]), 'Entities': int(hold[10])}
+                set = {'Source': hold[0], 'Length': hold[1], 'Word Count': hold[2], 'Positive': hold[3], 'Negative': hold[4], 'Neutral': hold[5], 'Compound': hold[6], 'Avg WordLen': int(hold[1]/hold[2]), 'UniGrams': int(hold[7]), 'BiGrams': int(hold[8]), 'TriGrams': int(hold[9]), 'Punctuation': int(hold[10]), 'Entities': int(hold[11]), 'Label': int(hold[12])}
                 clean_data.loc[len(clean_data)] = set
 print(clean_data.head())
-
-print("Finished without failure")
+clean_data.to_csv('SentimentFile', sep=',',index=False, encoding='utf-8')
+print("Done")
