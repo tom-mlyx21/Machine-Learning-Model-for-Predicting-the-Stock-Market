fc2212f81974d436b3b7d29ea2afad40dae552ac
Tom <tom40@aber.ac.uk>
2024-03-31 15:42:45 +0100

    Code Cleanup

--- Data Analysis.py
+++ Data Analysis.py
@@ -4,27 +4,17 @@ import nltk
 import string
 
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-from numpy.random import poisson
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import KFold, cross_val_score
-from sklearn.metrics import classification_report
 from sklearn.svm import SVC
 from sklearn.ensemble import VotingClassifier
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn import metrics
-from sklearn.metrics import confusion_matrix
-from sklearn.metrics import roc_auc_score
 import matplotlib.pyplot as plt
 from sklearn.metrics import RocCurveDisplay
 from sklearn.inspection import permutation_importance
 
-
-import warnings
-
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
 from sklearn.naive_bayes import GaussianNB
 from xgboost import plot_tree
 
@@ -35,13 +25,10 @@ nltk.download('omw-1.4')
 nltk.download('vader_lexicon')'''
 
 
-
-
 pd.options.mode.chained_assignment = None  # default='warn'
 
 # Load the model into pandas
 first_data = pd.read_csv('Combined_News_DJIA.csv')
-print(first_data)
 # Using a ready-made sentiment extractor to test against my model
 
 
@@ -51,7 +38,6 @@ sentiment_data = first_data.copy()
 # Since the original dataset is too large, I will give to option to test on the first 50
 def trainData():
     counter = 0
-    print()
     choice = input("Enter F for full data, or anything else for testing")
     if choice == 'F':
         boundary = len(first_data)
@@ -112,7 +98,7 @@ def frameBuilder(boundary):
                 if type(hold) == list:
                     dataPoints = {'Source': hold[0], 'Length': hold[1], 'Word Count': hold[2], 'Positive': hold[3], 'Negative': hold[4], 'Neutral': hold[5], 'Compound': hold[6], 'Avg WordLen': int(hold[1] / hold[2]), 'UniGrams': int(hold[7]), 'BiGrams': int(hold[8]), 'TriGrams': int(hold[9]), 'Punctuation': int(hold[10]), 'Entities': int(hold[11]), 'Label': int(hold[12])}
                     clean_data.loc[len(clean_data)] = dataPoints
-    print("clean data is: ", clean_data)
+    print("result ratio: ",  clean_data['Label'].value_counts(normalize=True))
     return clean_data
 
 # Program training section: Choose a training model, returns k-fold validation method results
@@ -156,6 +142,8 @@ def modelTrainer(clean_data):
             print(mean_absolute_error(Ytest, pred_final))
             print("Confusion Matrix: ", metrics.confusion_matrix(Ytest, pred_final))
             print("\n classification report: ", metrics.classification_report(Ytest, pred_final))
+
+
         if model == 'NB' or model == 'XGB' or model == 'RF' or model == 'SVM':
             clf.fit(Xtrain, Ytrain)
             Ypred = clf.predict(Xtest)
@@ -173,7 +161,6 @@ def modelTrainer(clean_data):
             sorted_idx = result.importances_mean.argsort()
             plt.boxplot(result.importances[sorted_idx].T, vert=False, labels=Xtest.columns[sorted_idx])
             plt.show()
-
         elif model != 'EN':
             print("Enter a Valid Model")
             modelTrainer(clean_data)
